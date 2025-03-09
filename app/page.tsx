efc9b745efbca3728d0e6a25c934931b3b4de1cd
imran-binhasan <imranbinhasan.work@gmail.com>
2025-03-09 09:37:36 +0600

    finish hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'> 
       <Hero/>
+      <section className='z-0 min-h-screen'></section>
     </main>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,51 +1,93 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 import Button from "./Button";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  useGSAP(()=>{
-    if(hasClicked){
-      gsap.set('#next-video', {visibility: 'visible'});
-      gsap.to('#next-video', {
-        transformOrigin:'center center',
-        scale:1,
-        width:'100%',
-        height:'100%',
-        duration:1,
-        ease:'power1.inOut',
-        onStart:() => nextVideoRef.current.play()
-      })
-      gsap.from('#current-video', {
-        transformOrigin: 'center center',
-        scale:0,
-        duration:1.5
-      })
-    }
-  },{dependencies:[currentIndex],revertOnUpdate:true})
   const totalVideos = 4;
   const nextVideoRef = useRef(null);
   const handleVideoLoad = () => {
+    console.log("Video loaded", loadedVideos + 1);
     setLoadedVideos((prev) => prev + 1);
   };
+
   const nextVideoIndex = (prevIndex: number) => (prevIndex % totalVideos) + 1;
   const handleMiniVideoPlayer = () => {
     setHasClicked(true);
     setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
   };
   const getVideoSource = (index: number) => `videos/hero-${index}.mp4`;
+  useEffect(() => {
+    setIsLoading(false)
+  }, [loadedVideos]);
 
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
 
+  useGSAP(
+    () => {
+      gsap.set("#video-frame", {
+        clipPath: "polygon(14% 0%, 75% 0%, 90% 90%, 0% 95%",
+      });
+
+      gsap.from("#video-frame", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%",
+        borderRadius: "0 0 0 0",
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: "#video-frame",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      });
+    },
+    { dependencies: [currentIndex] }
+  );
+
+  console.log(loadedVideos);
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -80,7 +122,7 @@ const Hero = () => {
             src={getVideoSource(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
@@ -98,13 +140,18 @@ const Hero = () => {
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
               Enter the Metagame Layer <br /> Unleash the Play Economy
             </p>
-            <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass="bg-yellow-300 flex-center gap-1"/>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="bg-yellow-300 flex-center gap-1"
+            />
           </div>
         </div>
       </div>
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black z-5">
-          G<b>a</b>ming
-        </h1>
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
