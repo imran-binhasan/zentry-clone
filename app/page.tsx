a52d2ee2d2d51fb3016737b24a89960e48b8ad53
imran-binhasan <imranbinhasan.work@gmail.com>
2025-03-09 06:20:14 +0600

    update layout and add hero section

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../components/Hero'
+const Home = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+      <Hero/>
+    </main>
+  )
+}
+
+export default Home
